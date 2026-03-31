8ead6a98033b73d9c0475b4b13b3344cc188fc1c
kmyop <daejung2607@dge.go.kr>
2026-03-31 10:30:40 +0900

    Add files via upload

--- S0303.java
+++ S0303.java
@@ -4,8 +4,10 @@ public class S0303 {
 	public static void main(String[] args) {
 		String a = "Hello java";
 		String b = "java";
-		System.out.println(a.equals(b));
-		System.out.println(a.indexOf("a"));
+		System.out.println(a.contains("java"));
+		System.out.println(a.charAt(5));
+		System.out.println(a.replaceAll("java", "world"));
+		System.out.println(a.substring(3, 8));
 	}
 }
 
