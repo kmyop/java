b09c715ab119cfc9c183b56b8e25e0c941655cd4
kmyop <daejung2607@dge.go.kr>
2026-04-03 11:29:53 +0900

    Add files via upload

---
+++ S0305.java
@@ -0,0 +1,12 @@
+package jump_to_java;
+
+public class S0305 {
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		sb.append("hello");
+		sb.append(" ");
+		sb.append("jump to java");
+		String result = sb.toString();
+		System.out.println(result);
+	}
+}
