cfe36c92d4b77b8e53bc0c05780ace0f6af3e019
kmyop <daejung2607@dge.go.kr>
2026-04-14 09:36:00 +0900

    Add files via upload

---
+++ S0308.java
@@ -0,0 +1,11 @@
+package jump_to_java;
+import java.util.HashMap;
+
+public class S0308 {
+	public static void main(String[] args) {
+		HashMap<String, String> map =new HashMap<>();
+		map.put("people","사람");
+		map.put("baseball","야구");
+		System.out.println(map.keySet());
+	}
+}
