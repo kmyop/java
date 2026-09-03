12017c15aea0ac14177f29e302c3f1c6de9f2e26
kmyop <daejung2607@dge.go.kr>
2026-09-03 09:25:34 +0900

    Add files via upload

---
+++ sksnrlProgram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace gameProject
+{
+    internal class Program
+    {
+        static void showData()
+        {
+            Console.WriteLine("외부데이터 없이 자체 동작");
+        }
+
+        static int devibe(int x, int y)
+        {
+            if (y != 0)
+            {
+                int retval;
+                retval = x / y;
+                Console.WriteLine("{0} {1}을 나눈 값: {2}", x, y, retval);
+                return retval;
+            }
+            else
+            {
+                Console.WriteLine("0으로 나눌 수 없음.");
+                return 0;
+            }
+
+        }
+
+                static void Main(string[] args)
+                {
+                    showData();
+                    devibe(7, 3);
+                    devibe(0, 3);
+                    devibe(7, 0);
+                }
+    }
+}
