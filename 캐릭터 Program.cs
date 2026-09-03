2240322a2833ed9f5e4b4dc168c96609abaed2f1
kmyop <daejung2607@dge.go.kr>
2026-09-03 09:36:04 +0900

    Add files via upload

---
+++ 캐릭터 Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace gameProject
+{
+    internal class Program
+    {
+        static void DisplayCharacter(String name, int point, String job) { 
+        
+            Console.WriteLine("===캐릭터 정보===");
+            Console.WriteLine("이름:" + name);
+            Console.WriteLine("포인트:" + point);
+            Console.WriteLine("직업:" + job);
+        }
+        static void Main(string[] args)
+        {
+            DisplayCharacter("슈퍼맨", 100, "기자");
+            DisplayCharacter(job: "학생", point: 150, name: "스마트");
+        }
+    }
+}
